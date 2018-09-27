022ab3e2f11d9fdb348e85cfbf89b6ff64ca93ba
jinhao <im7h1995@hotmail.com>
2018-09-27 15:03:06 +0800

    bug 修复

--- cleanblog/settings.py
+++ cleanblog/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myblog',
         'USER': 'root',
-        'PASSWORD': '#',
-        'HOST': '#',
+        'PASSWORD': 'im7h322000',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }

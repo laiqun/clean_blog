39ea7cec90c589416903c41ac63fba7f1314a044
jinhao <im7h1995@hotmail.com>
2018-09-29 12:44:16 +0800

    fix markdown css

--- cleanblog/settings.py
+++ cleanblog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_m9c!59*^eq8dpi9ntu$c0m32o4sgkaiu2ed$r@3^cftk@cbhs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Application definition
